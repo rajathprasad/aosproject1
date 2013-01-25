aa17ce43215a781e2fb5085ab3f4ca092491c8b3
Rajath Prasad <rajathprasad@gmail.com>
2013-01-24 21:11:01 -0500

    Updated test. tested switching contexts. Success

--- test.c
+++ test.c
@@ -1,1 +1,49 @@
 /* testing */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ucontext.h>
+
+struct gtthread_t
+{
+	long int id;
+	struct gtthread_t* next;  //pointer to next node
+	int terminted;	
+}gt[2];
+
+ucontext_t ctxt_main, ctxt_t1;
+	
+void func1()
+{
+	printf("func1:Swapped from main\n");
+	int a = 5;
+	swapcontext(&ctxt_t1, &ctxt_main);
+	printf("func1:back in func1\n");
+	printf("a is %d\n",a);
+	return;
+}
+
+void main()
+{
+	char main_stack[16384];
+	char stack1[16384];
+	
+
+	getcontext(&ctxt_t1);
+	ctxt_t1.uc_stack.ss_sp = stack1;
+        ctxt_t1.uc_stack.ss_size = sizeof(stack1);
+        ctxt_t1.uc_link = &ctxt_main;
+        makecontext(&ctxt_t1, func1, 0);
+	printf("In main: entering\n");
+	swapcontext(&ctxt_main, &ctxt_t1);
+	
+	printf("main: back in main\n");
+	swapcontext(&ctxt_main, &ctxt_t1);
+	printf("main:exiting main\n");
+	return;
+}
+	
+
+	
+
+
+
